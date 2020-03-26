40e135c9d826a1e8ebd03081d0b28939e406ae5c
Drew Nolan <drew.nolan@gmail.com>
2020-03-25 22:05:10 -0400

    tweak message

--- scrape/scrape.js
+++ scrape/scrape.js
@@ -1,14 +1,14 @@
 const reader = require("readline-sync");
 
 module.exports = {
-  'Demo test ecosia.org' : function (browser) {
+  'Scrape Alachua Clerk of Court' : function (browser) {
     browser
       .url('https://www.alachuaclerk.org/court_records/index.cfm')
       .waitForElementVisible('#captcha')
       .click('#captcha')
 
       .perform(function() {
-        const captchaWait = reader.question("Enter CAPTCHA Text on browser, then press ENTER");
+        const captchaWait = reader.question("Enter CAPTCHA Text on browser, then press ENTER in this window to continue...");
         console.log(`Thanks!`);
       })
 
