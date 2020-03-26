0583aece7aece2c247bfb4911096edaa9856e721
Drew Nolan <drew.nolan@gmail.com>
2020-03-25 21:46:34 -0400

    Get pause working

--- scrape/scrape.js
+++ scrape/scrape.js
@@ -1,14 +1,22 @@
+const reader = require("readline-sync");
+
 module.exports = {
-    'Demo test ecosia.org' : function (browser) {
-      browser
-        .url('https://www.ecosia.org/')
-        .waitForElementVisible('body')
-        .assert.titleContains('Ecosia')
-        .assert.visible('input[type=search]')
-        .setValue('input[type=search]', 'nightwatch')
-        .assert.visible('button[type=submit]')
-        .click('button[type=submit]')
-        .assert.containsText('.mainline-results', 'Nightwatch.js')
-        .end();
-    }
-  };
+  'Demo test ecosia.org' : function (browser) {
+    browser
+      .url('https://www.alachuaclerk.org/court_records/index.cfm')
+      .waitForElementVisible('#captcha')
+      .click('#captcha')
+
+      .perform(function() {
+        const captchaWait = reader.question("Enter CAPTCHA Text on browser, then press ENTER");
+        console.log(`Thanks!`);
+      })
+
+      .pause(50)
+      .click('#LoginForm input[type=submit]')
+      .pause(100)
+      .url('https://www.alachuaclerk.org/court_records/gis/')
+      .pause(10000)
+      .end();
+  }
+};
